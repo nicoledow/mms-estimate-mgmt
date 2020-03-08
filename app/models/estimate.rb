d60256d08b2f27f7a06423776e46de17fc638e29
Nicole Dow <nikki.dow71@gmail.com>
2020-03-08 15:48:04 -0700

    finalize

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -4,7 +4,7 @@ class Estimate < ApplicationRecord
     belongs_to :customer
 
     def estimated_labor_cost
-       if self.bedrooms == 1 || self.bedrooms == 2
+       if self.bedrooms == 1 
          return 75 * 2 * self.estimated_time_factor
        else
         return 75 * self.bedrooms * self.estimated_time_factor
@@ -14,15 +14,15 @@ class Estimate < ApplicationRecord
     def estimated_time_factor
         case self.parking_type
         when "driveway"
-            return 1
+            return 1 * self.floors
         when "10-20"
-            return 1.5
+            return 1.5 * self.floors
         when "30-40"
-            return 2
+            return 2 * self.floors
         when "farther"
-            return 2.5
+            return 2.5 * self.floors
         else
-            return 2
+            return 1 * self.floors
         end
     end
 
@@ -32,13 +32,14 @@ class Estimate < ApplicationRecord
         response = RestClient.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{start}&destinations=#{destination}&mode=driving&key=#{ENV['GOOGLE_MAPS_API_KEY']}")
         data = JSON.parse(response)
         driving_time = data["rows"][0]["elements"][0]["duration"]["text"] #string, ex: "1 day 16 hours"
-
         if driving_time.include?("day")
             days = driving_time[0].to_i
-            return days * 24
+            parsed_time = driving_time.split(" ")
+            hours = parsed_time[2].to_i
+            return days * 24 + hours
         else
             parsed_time = driving_time.split(" ")
-            return parsed_time[0].to_i
+            return parsed_time[0].to_i + 1
         end
     end
 
