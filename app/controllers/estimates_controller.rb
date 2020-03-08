bf2b7436db03ef1071eaa334bf77e8293da4f3fd
Nicole Dow <nikki.dow71@gmail.com>
2020-03-08 12:13:11 -0700

    add estimate instance methods

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -7,6 +7,24 @@ class EstimatesController < ApplicationController
 
     def create
         binding.pry
+        new_customer = Customer.create(
+            name: params[:customerName],
+            phone: params[:customerPhone],
+            email: params[:customerEmail]
+        )
+        new_estimate = Estimate.create(
+            starting_city: params[:startingCity],
+            starting_state: params[:startingState],
+            destination_city: params[:destinationCity],
+            destination_state: params[:destinationState],
+            bedrooms: params[:numOfBedrooms],
+            floors: params[:numOfFloors],
+            parking_type: params[:parkingDistance],
+            customer_id: new_customer.id,
+            quote: self.generate_quote
+        )
+
+        render json: new_estimate
     end
 
 private
--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,3 +1,22 @@
 class Estimate < ApplicationRecord
     belongs_to :customer
+
+    def calculate_labor_cost
+       if self.bedrooms == 1 || self.bedrooms == 2
+         return 75 * 2 * self.estimate_time
+       else
+        return 75 * self.bedrooms * self.estimate_time
+       end
+    end
+
+    def estimate_time
+        distance = self.calculate_distance
+        #multiply driving time * time factor based on parking situation and num of floors
+            #will need to call Google Maps API and use rest-client
+    end
+
+    def generate_quote
+        #use labor cost and time estimate to generate quote 
+            #save to db if not already saved
+    end
 end
