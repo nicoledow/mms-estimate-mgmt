5b7d0130b86e00c7cc8c3991497be3a4f33b18fe
Nicole Dow <nikki.dow71@gmail.com>
2020-03-09 20:52:30 -0700

    config dotenv-rails

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,13 @@ module MmsEstimateMgmt
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Bundler.require(*Rails.groups)
+
+    Dotenv::Railtie.load
+
+    HOSTNAME = ENV['HOSTNAME']
+
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
