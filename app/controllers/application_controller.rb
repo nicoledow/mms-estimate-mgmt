588abcdff73fa67975c50c6723036afbf9cf84a9
Nicole Dow <nikki.dow71@gmail.com>
2020-03-18 17:25:33 -0700

    sessions#create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
 private
     def require_login
         if !session[:user_id]
-        flash[:login_required] = "Log in required." 
-        redirect_to root_path
+            redirect_to root_path
         end
     end
 end
