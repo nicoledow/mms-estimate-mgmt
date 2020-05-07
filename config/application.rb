0d1fabd299f0379c16f0a0e3fdf34eed8a967f45
Nicole Dow <nikki.dow71@gmail.com>
2020-05-06 19:30:26 -0700

    reconfigure cors

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module MmsEstimateMgmt
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'mms-estimate-tool.herokuapp.com'
+        origins 'millennial-moving-solutions.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :patch, :options]
       end
     end
