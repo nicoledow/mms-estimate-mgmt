b4cbadc495fbce4c93048dffbd7226625d01bba2
Nicole Dow <nikki.dow71@gmail.com>
2020-05-10 08:56:19 -0700

    udpate gmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :user_name            => ENV['GMAIL_USERNAME'],
-  :password             => ENV['GMAIL_PASSWORD'],
+  :password             => ENV['GMAIL_PW'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
