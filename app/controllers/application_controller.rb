572839ad9762dac069b22bcc1c2b8cb410b224ef
Nicole Dow <nikki.dow71@gmail.com>
2021-01-30 09:49:45 -0700

    enable google sheets api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
 
+    def google_sheets_auth
+
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   resources :estimates, only: [:index, :show, :create]
 
+  get '/google/sheets', to: 'application#google_sheets_auth'
+  
   post '/distance', to: 'distances#create'
 end
